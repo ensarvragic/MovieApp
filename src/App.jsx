e48ce0fef8b1dba2799ac16763121fe6356396b2
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2023-08-30 03:28:48 +0200

    fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Axios from 'axios'
 import './App.css'
 
 function App() {
+  const [api, setApi] = useState("")
+
+  const fetchData = () => {
+    Axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=d8244dd6')
+    .then((response) => {
+      console.log(response);
+      const myRepo = response.data
+      setApi(myRepo)
+    })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
    <>React MovieApp</>
